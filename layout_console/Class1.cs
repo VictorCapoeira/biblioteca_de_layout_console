8ae6d7ac8e06b18a2a08ca396c73192907038aa7
Victor <victorgit28@outlook.com>
2025-02-10 10:37:40 -0300

    excluido estiloso

--- layout_console/Class1.cs
+++ layout_console/Class1.cs
@@ -166,15 +166,9 @@ namespace Layout_console
         public static void Minimalista(string titulo)
         {
 
-            Console.WriteLine(new string('=', titulo.Length + 6)); // Linha superior
-            Console.WriteLine($"== {titulo} ==");                 // Texto centralizado
-            Console.WriteLine(new string('=', titulo.Length + 6)); // Linha inferior
-        }
-        public static void Estiloso(string titulo)
-        {
-            Console.WriteLine("╔════════════════════╗");
-            Console.WriteLine($"║  {titulo.PadRight(16)}║");
-            Console.WriteLine("╚════════════════════╝");
+            Console.WriteLine(new string('=', titulo.Length + 6)); 
+            Console.WriteLine($"== {titulo} ==");                 
+            Console.WriteLine(new string('=', titulo.Length + 6)); 
         }
     }
 }
