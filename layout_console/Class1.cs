1fc49ca4efef9dc7b2fad307566b226057749431
Victor <victorgit28@outlook.com>
2025-02-10 08:27:32 -0300

    criado metodo para centralizar e colorir

--- layout_console/Class1.cs
+++ layout_console/Class1.cs
@@ -1,91 +1,175 @@
 ﻿using System;
-namespace layout_console{
-    public class LayoutWriteLine{
-        public static void Vermelho(string texto){
+namespace layout_console
+{
+    public class LayoutWriteLine
+    {
+        public static void Vermelho(string texto)
+        {
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine(texto);
             Console.ResetColor();
         }
-        public static void Amarelo(string texto){
+        public static void Amarelo(string texto)
+        {
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine(texto);
             Console.ResetColor();
         }
-        public static void Azul(string texto){
+        public static void Azul(string texto)
+        {
             Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine(texto);
             Console.ResetColor();
         }
-        public static void Verde(string texto){
+        public static void Verde(string texto)
+        {
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine(texto);
             Console.ResetColor();
         }
-        public static void Cinza(string texto){
+        public static void Cinza(string texto)
+        {
             Console.ForegroundColor = ConsoleColor.Gray;
             Console.WriteLine(texto);
             Console.ResetColor();
         }
-        public static void Preto(string texto){
+        public static void Preto(string texto)
+        {
             Console.ForegroundColor = ConsoleColor.Black;
             Console.WriteLine(texto);
             Console.ResetColor();
         }
     }
-    public class LayoutWrite{
-        public static void Vermelho(string texto){
+    public class LayoutWrite
+    {
+        public static void Vermelho(string texto)
+        {
             Console.ForegroundColor = ConsoleColor.Red;
             Console.Write(texto);
             Console.ResetColor();
         }
-        public static void Amarelo(string texto){
+        public static void Amarelo(string texto)
+        {
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.Write(texto);
             Console.ResetColor();
         }
-        public static void Azul(string texto){
+        public static void Azul(string texto)
+        {
             Console.ForegroundColor = ConsoleColor.Blue;
             Console.Write(texto);
             Console.ResetColor();
         }
-        public static void Verde(string texto){
+        public static void Verde(string texto)
+        {
             Console.ForegroundColor = ConsoleColor.Green;
             Console.Write(texto);
             Console.ResetColor();
         }
-        public static void Cinza(string texto){
+        public static void Cinza(string texto)
+        {
             Console.ForegroundColor = ConsoleColor.Gray;
             Console.Write(texto);
             Console.ResetColor();
         }
-        public static void Preto(string texto){
+        public static void Preto(string texto)
+        {
             Console.ForegroundColor = ConsoleColor.Black;
             Console.Write(texto);
             Console.ResetColor();
         }
     }
-    public class LayoutLinha{
-        public static void Completa(){
+    public class LayoutLinha
+    {
+        public static void Completa()
+        {
             Console.WriteLine(new string('-', Console.WindowWidth));
         }
-        public static void Meia(){
+        public static void Meia()
+        {
             Console.WriteLine(new string('-', Console.WindowWidth / 2));
         }
     }
-    public class LayoutPosicao{
-        public static void Centralizar(string texto, string cor = " "){
+    public class LayoutPosicao
+    {
+        public static void Centralizar(string texto, string cor = " ")
+        {
             cor = cor.ToLower();
-            switch(cor){
-                case "red" || "vermelho":
-                break;
+            switch (cor)
+            {
+                case "red":
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(new string(' ', Console.WindowWidth / 2) + texto);
+                    Console.ResetColor();
+                    break;
+                case "vermelho":
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(new string(' ', Console.WindowWidth / 2) + texto);
+                    Console.ResetColor();
+                    break;
+                case "yellow":
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine(new string(' ', Console.WindowWidth / 2) + texto);
+                    Console.ResetColor();
+                    break;
+                case "amarelo":
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine(new string(' ', Console.WindowWidth / 2) + texto);
+                    Console.ResetColor();
+                    break;
+                case "blue":
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine(new string(' ', Console.WindowWidth / 2) + texto);
+                    Console.ResetColor();
+                    break;
+                case "azul":
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine(new string(' ', Console.WindowWidth / 2) + texto);
+                    Console.ResetColor();
+                    break;
+                case "green":
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(new string(' ', Console.WindowWidth / 2) + texto);
+                    Console.ResetColor();
+                    break;
+                case "verde":
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(new string(' ', Console.WindowWidth / 2) + texto);
+                    Console.ResetColor();
+                    break;
+                case "gray":
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.WriteLine(new string(' ', Console.WindowWidth / 2) + texto);
+                    Console.ResetColor();
+                    break;
+                case "cinza":
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.WriteLine(new string(' ', Console.WindowWidth / 2) + texto);
+                    Console.ResetColor();
+                    break;
+                case "black":
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine(new string(' ', Console.WindowWidth / 2) + texto);
+                    Console.ResetColor();
+                    break;
+                case "preto":
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine(new string(' ', Console.WindowWidth / 2) + texto);
+                    Console.ResetColor();
+                    break;
+                default:
+                    Console.WriteLine(new string(' ', Console.WindowWidth / 2) + texto);
+                    break;
             }
-            Console.WriteLine(new string(' ', Console.WindowWidth / 2) + texto);
+
         }
-        public static void CentralizarLinha(string texto){
+        public static void CentralizarLinha(string texto)
+        {
             Console.WriteLine(new string('-', Console.WindowWidth / 2) + texto + new string('-', Console.WindowWidth / 2 - texto.Length));
         }
-        
-        public static void Esquerda(string texto){
+
+        public static void Esquerda(string texto)
+        {
             Console.WriteLine(new string(' ', Console.WindowWidth - texto.Length) + texto);
         }
     }
